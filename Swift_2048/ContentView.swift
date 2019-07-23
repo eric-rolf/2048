e675333ad0fc3b961efe688904d7fc141bacca1a
Eric Rolf <eric.rolf@53.com>
2019-07-23 10:32:43 -0400

    Initial Commit

---
+++ Swift_2048/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Swift_2048
+//
+//  Created by Rolf, Eric on 7/23/19.
+//  Copyright © 2019 Fifth Third Bank. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
